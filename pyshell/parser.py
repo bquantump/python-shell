575783b0849a1c02e2bd0391a911d97ed7361198
Yalin Li <yall@microsoft.com>
2020-07-27 18:44:19 -0700

    Add const values for python code input delimeters

--- pyshell/parser.py
+++ pyshell/parser.py
@@ -1,19 +1,21 @@
+from . import consts
+
 class Parser:
     def __init__ (self, input):
         self.input = input
 
     def checkBashOrPython(self):
         # Python var: '@'
-        if self.input[0] == '@':
+        if self.input[0] == consts.PYTHON_VAR_DELIMETER:
             #print('Get Python var: ', self.input[1:])
             return 'Python var ' + self.input[1:] #call pyRunner(self.input[1:])
         # Python single line: '>>>'
-        elif self.input[0:3] == '>>>': 
+        elif self.input[0:3] == consts.PYTHON_SINGLE_LINE_INPUT_DELEMETER: 
             #print('Get Python single line: ', self.input[3:])
             return 'Python single line ' + self.input[3:] #call pyRunner(self.input[2:])
         # Python multi line: '...'
-        elif self.input[0:3] == '...':             
-            inputSplit = self.input[3:].replace('...','\n\t') #get newlines in string instead of ...
+        elif self.input[0:3] == consts.PYTHON_MULTI_LINE_INPUT_DELIMETER:             
+            inputSplit = self.input[3:].replace(consts.PYTHON_MULTI_LINE_INPUT_DELIMETER,'\n\t') #get newlines in string instead of ...
             #print('Get Python multi line: (prints on newline to show tabs)\n', inputSplit)
             return 'Python multi line ' + inputSplit #call pyRunner(inputSplit)
         # all other commands must be bash/shell
