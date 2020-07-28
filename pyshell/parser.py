6f969aae8d049747690dba7d281657b74ec823b6
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2020-07-27 17:30:42 -0700

    removed egg folder and print
    removed egg folder and print

--- pyshell/parser.py
+++ pyshell/parser.py
@@ -5,20 +5,20 @@ class Parser:
     def checkBashOrPython(self):
         # Python var: '@'
         if self.input[0] == '@':
-            print('Get Python var: ', self.input[1:])
+            #print('Get Python var: ', self.input[1:])
             return 'Python var ' + self.input[1:] #call pyRunner(self.input[1:])
         # Python single line: '>>>'
         elif self.input[0:3] == '>>>': 
-            print('Get Python single line: ', self.input[3:])
+            #print('Get Python single line: ', self.input[3:])
             return 'Python single line ' + self.input[3:] #call pyRunner(self.input[2:])
         # Python multi line: '...'
         elif self.input[0:3] == '...':             
             inputSplit = self.input[3:].replace('...','\n\t') #get newlines in string instead of ...
-            print('Get Python multi line: (prints on newline to show tabs)\n', inputSplit)
+            #print('Get Python multi line: (prints on newline to show tabs)\n', inputSplit)
             return 'Python multi line ' + inputSplit #call pyRunner(inputSplit)
         # all other commands must be bash/shell
         else:
-            print('Bash/shell command: ', self.input)
+            #print('Bash/shell command: ', self.input)
             return 'Bash command ' + self.input #call shellRunner(self.input)
 
 # small tests
