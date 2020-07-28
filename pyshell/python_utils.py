3e44e8710459026efa756cd7a21879ed2fa0dba0
bquantump <53361486+bquantump@users.noreply.github.com>
2020-07-27 18:25:40 -0700

    WIP: shell for PyRunner

--- pyshell/python_utils.py
+++ pyshell/python_utils.py
@@ -7,3 +7,10 @@ class pyRunner:
     
     def run_py_script(self, path):
         out = runpy.run_path(file_path=path)
+
+    def run_python(self, py_code):
+        pass
+
+    def get_var(self, var_name):
+        pass
+    
