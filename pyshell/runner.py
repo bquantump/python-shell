b5e97b4a6025ff953504a406f0af09055c911d55
Yalin Li <yall@microsoft.com>
2020-07-27 18:45:37 -0700

    Add collecting user input

--- pyshell/runner.py
+++ pyshell/runner.py
@@ -1,2 +1,29 @@
+import os
+
+from . import consts, parser
+from sultan.api import Sultan
+
+
+def display_welcome():
+    print(' Welcome to pyshell')
+    print(' Useage:')
+    print('     Echo python variable with character @')
+    print("     Write single-line python code start with '>>>', if it is in multi-line start with '...'\n")
+
+
 def main():
-    pass
+    display_welcome()
+    while(True):
+        input = Sultan().stdin(os.getcwd() + ': ')
+        if input.startswith(consts.MULTI_LINE_PYTHON_INPUT_PREFIX):
+            while(True):
+                single_line_code = Sultan().stdin('')
+                # print('multi-line mode: '+input)
+                input += single_line_code
+                if single_line_code == consts.MULTI_LINE_PYTHON_INPUT_PREFIX:
+                    break
+        print(input) # pass the input to parser
+
+
+if __name__ == "__main__":
+    main()
