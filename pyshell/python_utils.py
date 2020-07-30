a2bbdadf878952a96efa862e74f67bb43ab36fc3
Yalin Li <yall@microsoft.com>
2020-07-29 19:26:52 -0700

    Add exception handlers in python runner

--- pyshell/python_utils.py
+++ pyshell/python_utils.py
@@ -6,10 +6,18 @@ class pyRunner:
         self.py_local_context = locals()
     
     def run_py_script(self, path):
-        out = runpy.run_path(path) 
+        try:
+            out = runpy.run_path(path)
+        except Exception as e:
+            print(e)
+            print('command failed!')
 
     def run_python(self, py_code):
-        exec(py_code, globals(), self.py_local_context)
+        try:
+            exec(py_code, globals(), self.py_local_context)
+        except Exception as e:
+            print(e)
+            print('command failed!')
 
     def get_var(self, var_name):
-        return (self.py_local_context.get(var_name) or globals().get(var_name)) 
+        return (self.py_local_context.get(var_name) or globals().get(var_name))
