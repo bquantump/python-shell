54fb2c1b839a6f26e4775d9dc985f731fa1285e5
bquantump <53361486+bquantump@users.noreply.github.com>
2020-07-29 18:22:13 -0700

    WIP: Fix tab stuff

--- pyshell/runner.py
+++ pyshell/runner.py
@@ -1,5 +1,6 @@
 import os
 import cmd
+import readline
 import sys
 from . import consts, parser
 from sultan.api import Sultan
@@ -46,7 +47,7 @@ class CmdParse(cmd.Cmd):
             output = self.shell_parser.checkBashOrPython(line)
             if output != None:
                 print(output)
-        self.prompt = colored(get_prefix(), 'cyan')
+            self.prompt = colored(get_prefix(), 'cyan')
     
     
     def do_py(self, arg):
@@ -57,8 +58,13 @@ class CmdParse(cmd.Cmd):
             self.multi_line = False
             self.prompt = colored(get_prefix(), 'cyan')
             self.line_buffer = ''
+            readline.parse_and_bind("tab: complete")
             return
         self.multi_line = True
+        self.old_completer = readline.get_completer()
+        self.completekey = 'o'
+        print(self.complete)
+        readline.parse_and_bind("tab: tab-insert")
         self.prompt = colored("python mode >>>", 'green') 
 
     def parseline(self, line):
@@ -79,6 +85,58 @@ class CmdParse(cmd.Cmd):
         while i < n and line[i] in self.identchars: i = i+1
         cmd, arg = line[:i], line[i:].strip()
         return cmd, arg, line
+    
+    def cmdloop(self, intro=None):
+        """Repeatedly issue a prompt, accept input, parse an initial prefix
+        off the received input, and dispatch to action methods, passing them
+        the remainder of the line as argument.
+
+        """
+
+        self.preloop()
+        if self.use_rawinput and self.completekey:
+            try:
+                import readline
+                self.old_completer = readline.get_completer()
+                readline.set_completer(None)
+                readline.parse_and_bind(self.completekey+": complete")
+            except ImportError:
+                pass
+        try:
+            if intro is not None:
+                self.intro = intro
+            if self.intro:
+                self.stdout.write(str(self.intro)+"\n")
+            stop = None
+            while not stop:
+                if self.cmdqueue:
+                    line = self.cmdqueue.pop(0)
+                else:
+                    if self.use_rawinput:
+                        try:
+                            line = input(self.prompt)
+                        except EOFError:
+                            line = 'EOF'
+                    else:
+                        self.stdout.write(self.prompt)
+                        self.stdout.flush()
+                        line = self.stdin.readline()
+                        if not len(line):
+                            line = 'EOF'
+                        else:
+                            line = line.rstrip('\r\n')
+                line = self.precmd(line)
+                stop = self.onecmd(line)
+                stop = self.postcmd(stop, line)
+            self.postloop()
+        finally:
+            if self.use_rawinput and self.completekey:
+                try:
+                    import readline
+                    readline.set_completer(self.old_completer)
+                except ImportError:
+                    pass
+
 
 if __name__ == "__main__":
     main()
